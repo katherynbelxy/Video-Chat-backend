eb093625dc388f78d04942054ff2e9740f660c83
katherynbelxy <36305427+katherynbelxy@users.noreply.github.com>
2024-10-22 16:49:52 -0500

    r

--- api/index.js
+++ api/index.js
@@ -1,56 +1,50 @@
 
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const cors = require('cors');
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-// Configurar CORS
-app.use(cors({
-    origin: 'https://video-chat-frontend-one.vercel.app',
-    methods: ['GET', 'POST'],
-    credentials: true
-}));
-
-app.get('/', (req, res) => {
-    res.send('eServidor de Video Chat funcionando');
+;const express = require('express');
+  const http = require('http');
+  const socketIo = require('socket.io');
+  const cors = require('cors');
+  
+  const app = express();
+  const server = http.createServer(app);
+  const io = socketIo(server, {
+    cors: {
+      origin: 'https://video-chat-frontend-one.vercel.app', // Cambia esto a tu URL de frontend
+      methods: ['GET', 'POST'],
+      credentials: true
+    }
   });
-let users = {};
-
-io.on('connection', (socket) => {
+  
+  app.use(cors());
+  app.get('/', (req, res) => {
+    res.send('3Servidor de Video Chat funcionando');
+  })
+  io.on('connection', (socket) => {
     console.log('Usuario conectado:', socket.id);
-    
-    // Almacena la ID del usuario
-    users[socket.id] = socket.id;
-
+  
     socket.emit('me', socket.id);
-    socket.broadcast.emit('partnerId', socket.id);
-
+    
     socket.on('join', () => {
-        console.log('Usuario se unió:', socket.id);
+      console.log('Usuario se unió a la llamada:', socket.id);
+      socket.broadcast.emit('partnerId', socket.id);
     });
-
-    socket.on('signal', (data) => {
-        console.log('Señal enviada a:', data.to);
-        socket.to(data.to).emit('signal', { signal: data.signal, from: socket.id });
+  
+    socket.on('signal', ({ to, signal }) => {
+      console.log(`Enviando señal a ${to}:`, signal);
+      socket.to(to).emit('signal', { signal, from: socket.id });
     });
-
+  
     socket.on('message', (message) => {
-        console.log('Mensaje enviado:', message);
-        socket.broadcast.emit('message', message);
+      console.log('Mensaje enviado:', message);
+      socket.broadcast.emit('message', message);
     });
-
+  
     socket.on('disconnect', () => {
-        console.log('Usuario desconectado:', socket.id);
-        delete users[socket.id];
-        socket.broadcast.emit('user-disconnected', socket.id);
+      console.log('Usuario desconectado:', socket.id);
     });
-});
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
-});
+  });
+  
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+  });
+  
